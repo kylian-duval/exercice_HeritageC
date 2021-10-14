c56ab0af566b9b79ff3aa7d5ee0c960c750d0562
kylian-duval <70692979+kylian-duval@users.noreply.github.com>
2021-10-14 08:24:39 +0200

    Add files via upload

---
+++ exercice 1/exercice 1/dé.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include <random>
+class dé
+{
+private:
+	int De = 0;
+	int score = 0;
+	int NbHistorique = 1;
+public:
+	void ajoutauscore();
+	void resetScore();
+	void de();
+	int getDé();
+	int getScore();
+	void nombreHistorique();
+	int getNbHistorique();
+
+};
+
---
+++ exercice 1/exercice 1/main.cpp
@@ -0,0 +1,11 @@
+#include "exercice1.h"
+#include "dé.h"
+#include <QtWidgets/QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    exercice1 w;
+    w.show();
+    return a.exec();
+}
