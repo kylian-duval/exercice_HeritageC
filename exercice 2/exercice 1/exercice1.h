70e83f1ef080e4af8b08ec47cfa5d8fa5c54e94b
kylian-duval <70692979+kylian-duval@users.noreply.github.com>
2021-10-14 08:22:32 +0200

    Add files via upload

---
+++ exercice 2/exercice 1/exercice1.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <QtWidgets/QMainWindow>
+#include "ui_exercice1.h"
+#include "dé.h"
+#include "dehisto.h"
+
+
+class exercice1 : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    exercice1(QWidget *parent = Q_NULLPTR);
+	void MenuPrincipal();
+	
+private:
+	dé * DE = new dé();
+	dehisto * DEHISTO = new dehisto();
+    Ui::exercice1Class ui;
+	void afficheScore();
+public slots:
+	void lanceDe();
+	void afficheDe();
+	void resetScore();
+	void AffichedeNormal();
+	void AfficheDeHisto();
+	void Nlancer();
+};
